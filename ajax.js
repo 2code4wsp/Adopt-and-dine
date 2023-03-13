b93bae44be9f06d5558a3d546db2c36ec6e9de8a
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-03-13 12:29:15 -0700

    changed shelter code to only provide shelters and got shelter data to append to card and display on webpage

--- ajax.js
+++ ajax.js
@@ -1,135 +1,114 @@
-
-
 const petKey = 'vO3ybpsfJI6gi3UQ4bPmLW91dFsM8zOh5TsgnjjRQY0sTkMggW'
 const petSecret = 'bx9RsyfRlesCpNa5SnRXTox2w2NvWSOSYy8MWGVf'
-const dogURL = `https://api.petfinder.com/v2/animals?type=dog&location=97215&limit=3&fields=name,age,gender,breeds,photos`;
+//const dogURL = `https://api.petfinder.com/v2/animals?type=dog&location=97215&limit=3&fields=name,age,gender,breeds,photos`
 
 //On click of the button
-$('.btn').click(function(){
-    //get location from zipcode input
-    var location = $('#input').val().trim();
-    //get access Token to access information from api
-    getToken();
-    //get dogs from input zipcode
-    getDogs(location);
-    //get VetShelters from input zipcode
-    getVetShelters(location);
-    //get restaurants from input zipcode
-    //getRestaurant(location);
-});
+$('.btn').click(function () {
+  //get location from zipcode input
+  var location = $('#input').val().trim()
+  //get access Token to access information from api
+  getToken()
+  //get dogs from input zipcode
+  getDogs(location)
+  //get VetShelters from input zipcode
+  getShelters(location)
+  //get restaurants from input zipcode
+  //getRestaurant(location);
+})
 
-
-function getToken() {
-    // Make a request to the authentication endpoint to obtain a new token
-    $.ajax({
-       type: "POST",
-       url: "https://api.petfinder.com/v2/oauth2/token",
-       data: {
-         grant_type: "client_credentials",
-         client_id: "vO3ybpsfJI6gi3UQ4bPmLW91dFsM8zOh5TsgnjjRQY0sTkMggW",
-         client_secret: "bx9RsyfRlesCpNa5SnRXTox2w2NvWSOSYy8MWGVf",
-       }
-     })
-     .done(function(response) {
-         console.log(response);
-       authToken = response.access_token;
-       console.log(authToken);
-       localStorage.setItem('authToken',response.access_token), 
-       // Set the expiration time for the new token to one hour from the current time
-       authTokenExpirationTime = new Date().getTime() + response.expires_in * 1000;
-    });
-   }
-
-
+function getToken () {
+  // Make a request to the authentication endpoint to obtain a new token
+  $.ajax({
+    type: 'POST',
+    url: 'https://api.petfinder.com/v2/oauth2/token',
+    data: {
+      grant_type: 'client_credentials',
+      client_id: 'vO3ybpsfJI6gi3UQ4bPmLW91dFsM8zOh5TsgnjjRQY0sTkMggW',
+      client_secret: 'bx9RsyfRlesCpNa5SnRXTox2w2NvWSOSYy8MWGVf'
+    }
+  })
+  .done(function (response) {
+    console.log(response)
+    authToken = response.access_token
+    console.log(authToken)
+    localStorage.setItem('authToken', response.access_token),
+      // Set the expiration time for the new token to one hour from the current time
+      (authTokenExpirationTime =
+        new Date().getTime() + response.expires_in * 1000)
+  })
+}
 
 //get auth token from storage
- let authToken = localStorage.getItem('authToken')
+let authToken = localStorage.getItem('authToken')
 
-
-
- function getDogs(location) {
-        
-         $.ajax({
-         url: 'https://api.petfinder.com/v2/animals?type=dog&status=adoptable&has_photo=1',
-         type: 'GET',
-         headers: {
-             'Authorization': `Bearer ${authToken}`
-         },
-         data: {
-             'type': 'dog',
-             'location': location,
-             'limit': '5',
-             'fields': 'name,age,gender,breeds,photos'
-         }
-     })
-     .done(function(data) {
-         console.log(data);
-         const dogContainer = $('#additionalDogEl');
-         data.animals.forEach(dog => {
-             const card = $('<div>').addClass('card');
-             const name = $('<h2>').text(dog.name);
-             const age = $('<p>').text(`Age: ${dog.age}`);
-             const gender = $('<p>').text(`Gender: ${dog.gender}`);
-             const breed = $('<p>').text(`Breed: ${dog.breeds.primary}`);
-             const photo = $('<img>').attr('src', dog.photos[0].medium);
-             card.append(name, age, gender, breed, photo);
-             dogContainer.append(card);
-         });
-     })
-     .fail(function(jqXHR, textStatus, errorThrown) {
-         console.log(`AJAX request failed: ${textStatus}, ${errorThrown}`);
-     });
-    
-     }
-    
-
-    const dogOfficesURL = `https://api.petfinder.com/v2/organizations?type=vet,shelter&location=97215&limit=5`;
-
-    function getVetShelters (location, limit) {
-        $.ajax({
-            url: dogOfficesURL,
-            headers: {
-                Authorization: `Bearer ${authToken}`
-            },
-            method: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                console.log(data);
-                //handle vet shelter data
-                const vetContainer = $('#vetEl');
-                const shelterContainer = $('#shelterEl');
-        
-                $.each(data.organizations, function(index, org) {
-                    //create a card forEach
-                    const card = $('<div>').addClass('card');
-        
-                    //add org info to card
-                    const name = $('<h2>').text(org.name);
-        
-                    const address = $('<p>').text(`Address: ${org.address.address1}, ${org.address.city}, ${org.address.state}, ${org.address.postcode}`);
-        
-                    const phone = $('<p>').text(`Phone: ${org.phone}`);
-        
-                    const website = $('<a>').attr('href', org.website).text(org.website);
-        
-                    //add elements to card
-                    card.append(name);
-                    card.append(address);
-                    card.append(phone);
-                    card.append(website);
-        
-                    //place card in correct container
-                    if (org.type === 'vet') {
-                        vetContainer.append(card);
-                    } else if (org.type === 'shelter') {
-                        shelterContainer.append(card);
-                    }
-                });
-            },
-            error: function(error) {
-                console.error(error);
-            }
-        });
+function getDogs (location) {
+  $.ajax({
+    url: 'https://api.petfinder.com/v2/animals?type=dog&status=adoptable&has_photo=1',
+    type: 'GET',
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    },
+    data: {
+      type: 'dog',
+      location: location,
+      limit: '5',
+      fields: 'name,age,gender,breeds,photos'
     }
+  })
+    .done(function (data) {
+      console.log(data)
+      const dogContainer = $('#additionalDogEl')
+      data.animals.forEach(dog => {
+        const card = $('<div>').addClass('card')
+        const name = $('<h2>').text(dog.name)
+        const age = $('<p>').text(`Age: ${dog.age}`)
+        const gender = $('<p>').text(`Gender: ${dog.gender}`)
+        const breed = $('<p>').text(`Breed: ${dog.breeds.primary}`)
+        const photo = $('<img>').attr('src', dog.photos[0].medium)
+        card.append(name, age, gender, breed, photo)
+        dogContainer.append(card)
+      })
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      console.log(`AJAX request failed: ${textStatus}, ${errorThrown}`)
+    })
+}
 
+const dogOfficesURL = `https://api.petfinder.com/v2/organizations?type=shelter&limit=5`
 
+function getShelters (location) {
+  $.ajax({
+    url: dogOfficesURL,
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    },
+    method: 'GET',
+    dataType: 'json',
+    data: {
+        type: 'dogshelter',
+        location: location,
+        limit: '5',
+        fields: 'name, address, phone, url'
+      }
+    })
+    .done(function (data) {
+        console.log(data)
+        const shelterContainer = $('#shelterEl')
+  
+        data.organizations.forEach(organizations =>{
+         //create a card forEach
+          const card = $('<div>').addClass('card')
+          //add org info to card
+          const name = $('<h2>').text(organizations.name)
+          const address = $('<p>').text(`Address: ${organizations.address}`)
+          const phone = $('<p>').text(`Phone: ${organizations.phone}`)
+          const website = $('<a>').attr('href', organizations.website).text(organizations.website)
+          card.append(name, address, phone, website)
+          shelterContainer.append(card)            
+        })  
+})
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      console.log(`AJAX request failed: ${textStatus}, ${errorThrown}`)
+  
+    })
+}
